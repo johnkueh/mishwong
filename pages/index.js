60159209dda4a96ca70d59a8488d775e2f77645b
John Kueh <yoongfook@gmail.com>
2019-08-10 14:31:29 +1000

    First commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import Head from "../components/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head title="Mishwong" />
+      <h1>Coming soon</h1>
+    </div>
+  );
+};
+
+export default Home;
