d1f19a34e4f4a6c5de342c1b3073d266a7fc2188
John Kueh <yoongfook@gmail.com>
2019-08-18 14:22:32 +1000

    Updated links

--- components/footer.js
+++ components/footer.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
 
 const Footer = () => (
   <div className="flex justify-between pt-8 pb-16">
     <div>
-      <a className="mr-5" href="#">
-        Email
-      </a>
-      <a className="mr-5" href="#">
-        Dribbble
-      </a>
-      <a className="mr-5" href="#">
-        LinkedIn
-      </a>
-      <a className="mr-5" href="#">
-        GitHub
-      </a>
+      <Link href="mailto:michele257@gmail.com">
+        <a target="new" className="mr-5" href="mailto:michele257@gmail.com">
+          Email
+        </a>
+      </Link>
+      <Link href="https://dribbble.com/mishwong">
+        <a target="new" className="mr-5" href="https://dribbble.com/mishwong">
+          Dribbble
+        </a>
+      </Link>
+      <Link href="https://dribbble.com/mishwong">
+        <a target="new" className="mr-5" href="https://dribbble.com/mishwong">
+          LinkedIn
+        </a>
+      </Link>
     </div>
   </div>
 );
